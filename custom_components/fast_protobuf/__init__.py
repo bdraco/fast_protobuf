8be9c25ca3eb798ae1ea3792774c1d2fa5d5536a
J. Nick Koston <nick@koston.org>
2023-01-03 11:38:32 -1000

    fixes

--- custom_components/fast_protobuf/__init__.py
+++ custom_components/fast_protobuf/__init__.py
@@ -77,9 +77,11 @@ def build_wheel(target_dir: str, version: str) -> str:
             f"cd {tmp_dist_dir}/protobuf && git submodule update --init --recursive --depth 1"
         )
         run_command(
-            f"cd {tmp_dist_dir}/protobuf && cmake -Dprotobuf_BUILD_EXAMPLES=OFF -Dprotobuf_BUILD_TESTS=OFF ."
+            f"cd {tmp_dist_dir}/protobuf && CFLAGS='-fPIC' CXXFLAGS='-fPIC' cmake -Dprotobuf_BUILD_EXAMPLES=OFF -Dprotobuf_BUILD_TESTS=OFF ."
         )
-        run_command(f"cd {tmp_dist_dir}/protobuf && cmake --build . --parallel 10")
+        run_command(
+            f"cd {tmp_dist_dir}/protobuf && CFLAGS='-fPIC' CXXFLAGS='-fPIC' cmake --build . --parallel 10"
+        )
         run_command(f"cd {tmp_dist_dir}/protobuf/src && ln -s ../ .libs")
         run_command(
             f"cd {tmp_dist_dir}/protobuf/python && "
